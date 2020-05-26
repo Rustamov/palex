cafb103460e7fd0cdff39a08c4093dbe361687fc
Kemran Rustamov <Rustamovkm@gmail.com>
2020-05-26 19:39:20 +0300

    daily save

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 svg4everybody(); //for svg spite in ie
-objectFitImages();
+// objectFitImages();
 
-
-let $body,
+let $window,
+    $body,
     wWidth = 0,
     wHeight = 0,
     W_SM = 576,
@@ -18,5 +18,88 @@ let $body,
 $(document).ready(function () {
     $body = $('body');
     
+    $body = $('body');
+    $window = $(window);
+
+    wWidth =  $(window).width();
+    wHeight =  $(window).height();
+
+    $window.on('resize', function() {
+        wWidth =  $window.width();
+        wHeight =  $window.height();
+    });
+
+    headertMenu();
+
+    function headertMenu () {
+        let headerNav = $('.header-nav'),
+            headerNavIsOpen = headerNav.hasClass('open'),
+            openClass = 'header-nav-open',
+            opening = false,
+            transitionTime = 500,
+            timeout;
+
+
+
+        $body.on('click touch', '.js-header-nav-trigger', function (e) {
+            e.preventDefault();
+            navToggle();
+        });
+
+
+        $body.on('click touch', function (event) {
+            let obj = $(event.target);
+
+            if ( !opening && headerNav.hasClass('open') && !obj.closest('.header-nav').length && !obj.closest('.fancybox-container').length ) {
+                navClose();
+            };
+        });
+
+        $body.on('keydown', function(e) {
+            if ( !opening && headerNavIsOpen && (e.keyCode  === 27)) { // escape key maps to keycode '27'
+                navToggle()
+            };
+        });
+
+        function navToggle() {
+            if ( opening ) {
+                return 
+            }
+
+            opening = true;
+
+            headerNavIsOpen = headerNav.hasClass('open');
+
+            headerNav.toggleClass('open', !headerNavIsOpen);
+
+            if (!headerNavIsOpen) {
+                $body.toggleClass(openClass, true);
+            }
+        
+            if ( timeout ) {
+                clearTimeout(timeout)
+            }
+
+            timeout = setTimeout(function() {
+                headerNavIsOpen = headerNav.hasClass('open');
+
+                if (!headerNavIsOpen) {
+                    $body.toggleClass(openClass, false);
+                }
+                opening = false;
+            }, transitionTime)
+            
+        };
+
+        $('.header-nav__content').on('scroll',function(e) {
+            if ( $('.header-nav__content').scrollTop() > 10 ) {
+                $body.addClass('header-nav-scroll');
+            } else {
+                $body.removeClass('header-nav-scroll');
+            }
+        });
+    };
+
+
 });
 
